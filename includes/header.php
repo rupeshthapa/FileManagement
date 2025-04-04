948d1f0c2c4ea1f6cd2a7f566e4b81d241db3dec
Rupesh Thapa <thaparupesh767@gmail.com>
2025-04-05 00:03:25 +0545

    starting for ui

---
+++ includes/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>File Management</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="styles.css">
+    </head>
+    <body>
+        <div class="container">
+        <header>
+            <div class="header-section">
+            <h2>My Portfolio</h2>
+            <nav>
+                <ul>
+                    <li><a href="home.html">Home</a></li>
+                    <li><a href="about.html">About</a></li>
+                    <li><a href="contact.html">Contact</a></li>
+                </ul>
+            </nav>
+            </div>
+        </header>
